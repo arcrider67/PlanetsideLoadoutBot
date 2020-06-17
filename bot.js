a916448cb4deab8119c7965bec30316cb24ec239
arcrider67 <allenjany67@gmail.com>
2020-06-16 21:57:00 -0400

    added commands

--- bot.js
+++ bot.js
@@ -1,24 +1,86 @@
 require('dotenv').config();
+const fetch = require("node-fetch");
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const PS2endpoint ="http://census.daybreakgames.com/get/ps2:v2/"
+const CharIDSearch = "/character/?name.first="
+const ItemGet = "characters_item?character_id="
+const ItemGetParams = "&c:limit=2000"
+
+
+
+//startup 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
+
+client.commands = new Discord.Collection();
+const botCommands = require('./commands');
+
+Object.keys(botCommands).map(key => {
+  client.commands.set(botCommands[key].name, botCommands[key]);
+});
+console.log(client.commands);
+
 client.on('message', msg => {
 
-    console.log(msg.author.id);
+    if(msg.author.id == process.env.CLIENT_ID) return;
+
+    const args = msg.content.split(/ +/);
+    const command = args.shift().toLowerCase();
+    console.info(`Called command: ${command}`);
+  
+    if (!client.commands.has(command)){
+        msg.reply('Unknown Command please use !commandname or !help to list commands');
+        return;
+    } 
+  
+    try {
+        client.commands.get(command).execute(msg, args);
+        
+    } catch (error) {
+      console.error(error);
+      msg.reply('there was an error trying to execute that command!');
+    }
+
+});
+
+/*
+//when message detected in bot's channel
+client.on('message', msg => {
 
     if (msg.content === 'ping') {
         msg.reply('pong');
     
         //check if the command is valid and it was not sent from this bot
     } else if (msg.content.includes("!check") && msg.author.id != process.env.CLIENT_ID){
-        msg.reply("check what?");
+        //msg.reply("check what?");
+        var url = PS2endpoint + CharIDSearch;
+    
+        fetch(url) // Call the fetch function passing the url of the API as a parameter
+        .then((resp) => resp.json())
+    
+        .then(function(data) {
+            // Your code for handling the data you get from the API
+            console.log(data);
+            var charID = data["character_list"][0]["character_id"];
+            console.log(charID);
+            url =   
+        })
+        .catch(function(error) {
+        // This is where you run code if the server returns any errors
+        });
+    
+    
     }
 
-  });
+    /*
+    */
+
+
+
---
+++ commands/check.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: '!check',
+    description: 'Use to check a players owned items for a given player. Usage: !check playername class/specialization',
+    execute(msg, args) {
+
+        const fetch = require("node-fetch");
+
+        const PS2endpoint ="http://census.daybreakgames.com/get/ps2:v2/"
+        const CharIDSearch = "/character/?name.first="
+        const ItemGet = "characters_item?character_id="
+        const ItemGetParams = "&c:limit=2000"
+
+        //msg.reply("check what?");
+        var url = PS2endpoint + CharIDSearch + args[1];
+        
+        fetch(url) // Call the fetch function passing the url of the API as a parameter
+        .then((resp) => resp.json())
+    
+        .then(function(data) {
+            // Your code for handling the data you get from the API
+            var charID = data["character_list"][0]["character_id"];
+            console.log(charID);
+            url = PS2endpoint + ItemGet + charID + ItemGetParams;
+            console.log(url);
+            fetch(url)
+            .then((resp) => resp.json())
+            .then(function(data){
+                console.log(data);
+            })
+        })
+        .catch(function(error) {
+        // This is where you run code if the server returns any errors
+        });
+        console.log("Command Complete");
+    },
+  };
