7687843c6fbac9bc3465e642bf292dfb50927f82
Allen <allen@Allens-MacBook-Pro.local>
2020-08-13 16:07:13 -0400

    Added Medic class to Loadouts

--- commands/LoadoutManager/newLoadout.js
+++ commands/LoadoutManager/newLoadout.js
@@ -1,8 +1,26 @@
+const fs = require('fs');
+
 module.exports = {
     name: '!newLoadout',
     description: 'adds a new loadout to the outfit that is calling the command',
     execute(msg, args){
 
+        const guildID = msg["guild"]["id"]
+        const guildName = msg["guild"]["name"]
+        const dir = "Outfits/"
+
+
+        fs.readFile(dir+guildID+".json", 'utf8', function read(err, data){
+            //read in the entire database for the guild
+
+            if(err){
+                throw err;
+            }
+            const content = data;
+
+            processFile(content);
+
+        });
 
 
 
@@ -12,3 +30,7 @@ module.exports = {
 
 
 };
+
+function processFile(content){
+    console.log(content)
+}
